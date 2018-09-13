a7a6d36a419e7e11529dbd2bf409d256d9659188
Wess58 <wesleymbate@gmail.com>
2018-09-13 10:30:27 +0300

    Added bootstrap to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DiaryComponent } from './diary/diary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiaryComponent
   ],
   imports: [
     BrowserModule
