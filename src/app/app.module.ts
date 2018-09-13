752f6fda94a01115c0528964cce6f48b342f497a
Wess58 <wesleymbate@gmail.com>
2018-09-13 11:04:55 +0300

    Added Formmodule imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DiaryComponent } from './diary/diary.component';
+import { DiaryFormComponent } from './diary-form/diary-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DiaryComponent
+    DiaryComponent,
+    DiaryFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
